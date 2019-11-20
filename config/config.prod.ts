c990bf3c02ebd230238d3cbb5bd87d786002a184
Tesla <547249523@qq.com>
2019-11-20 18:18:27 +0800

    fix: 数据库脚本

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -2,5 +2,30 @@ import { EggAppConfig, PowerPartial } from 'egg'
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {}
+  config.mysql = {
+    client: {
+      host: 'localhost',
+      port: '3306',
+      user: 'root',
+      password: '123456',
+      database: 'pl_icon'
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false
+  }
+  config.redis = {
+    client: {
+      port: 6379,          // Redis port
+      host: '127.0.0.1',   // Redis host
+      password: '123456',
+      db: 0
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false
+  }
   return config
 }
--- database/init.sql
+++ database/init.sql
@@ -4,14 +4,14 @@ use pl_icon;
 
 
 # 用户表
-CREATE TABLE user (
-	id VARCHAR(32) PRIMARY KEY NOT NULL,
-	account VARCHAR(100) NOT NULL, # 账户
-	password VARCHAR(32) NOT NULL, # 密码
-	nickname VARCHAR(100), # 昵称
-	avatar VARCHAR(200), # 头像
-	create_time datetime NOT NULL DEFAULT NOW()
-);
+#CREATE TABLE user (
+#	id VARCHAR(32) PRIMARY KEY NOT NULL,
+#	account VARCHAR(100) NOT NULL, # 账户
+#	password VARCHAR(32) NOT NULL, # 密码
+#	nickname VARCHAR(100), # 昵称
+#	avatar VARCHAR(200), # 头像
+#	create_time datetime NOT NULL DEFAULT NOW()
+#);
 
 # 项目表
 CREATE TABLE project (
@@ -25,10 +25,12 @@ CREATE TABLE project (
 # 图标表
 CREATE TABLE icons (
 	id VARCHAR(32) PRIMARY KEY NOT NULL,
+	project_id VARCHAR(32),
 	content TEXT, # 图标内容
 	icon_name VARCHAR(50) NOT NULL, # 图标名称
 	icon_desc VARCHAR(50),	# 图标描述
-	namespaec VARCHAR(50) # 命名空间
+	visible INT,
+	namespace VARCHAR(50) # 命名空间
 );
 
 # 图标打包后的链接
@@ -42,10 +44,10 @@ CREATE TABLE link (
 )
 
 # 操作日志
-CREATE TABLE logs (
-	id VARCHAR(32) PRIMARY KEY NOT NULL,
-	classify INT NOT NULL,	# 分类： 0 项目，1 图标
-	type INT NOT NULL, # 操作类型：0 新增 1 删除 2 修改
-	target_id VARCHAR(32), # 操作目标的id
-	update_time datetime NOT NULL DEFAULT NOW() # 操作时间
-)
+#CREATE TABLE logs (
+#	id VARCHAR(32) PRIMARY KEY NOT NULL,
+#	classify INT NOT NULL,	# 分类： 0 项目，1 图标
+#	type INT NOT NULL, # 操作类型：0 新增 1 删除 2 修改
+#	target_id VARCHAR(32), # 操作目标的id
+#	update_time datetime NOT NULL DEFAULT NOW() # 操作时间
+#)
